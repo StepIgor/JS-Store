6bcd9eaeb0637e7caae9bdf6ae35770bd18ce482
StepIgor <igor.stepanov@difres.ru>
2020-03-25 16:13:46 +0300

    not provided

--- js/start.js
+++ js/start.js
@@ -18,15 +18,3 @@ for (elem = Object.keys(db).length; elem > Object.keys(db).length-6; elem--){
     document.getElementById('apps_in_store').innerHTML += '<div style="padding:0;cursor:pointer;" onclick="document.location.href = \'product.html?id='+elem+'\'" class="card col s4 m2 l2 hoverable"><div class="card-image"><img class="responsive-img" src="img/apps_screenshots/'+elem+'/0.png"></div><div class="card-content" style="padding-top:5px;padding-left:15px;"><b>'+db[elem]['product_name']+'</b><br><span class="grey-text">'+developers[db[elem]['developer_id']]['username']+'</span></div></div>';
   }
 }
-
-
-//show games
-
-for (elem = Object.keys(db).length; elem > Object.keys(db).length-6; elem--){
-  if (elem < 1){
-    break;
-  }
-  if (db[elem]['type'] == 2){
-    document.getElementById('apps_in_store').innerHTML += '<div style="padding:0;cursor:pointer;" onclick="document.location.href = \'product.html?id='+elem+'\'" class="card col s4 m2 l2 hoverable"><div class="card-image"><img class="responsive-img" src="img/apps_screenshots/'+elem+'/0.png"></div><div class="card-content" style="padding-top:5px;padding-left:15px;"><b>'+db[elem]['product_name']+'</b><br><span class="grey-text">'+developers[db[elem]['developer_id']]['username']+'</span></div></div>';
-  }
-}
